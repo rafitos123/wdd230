b06cc5986411c6ae997a4d1d1f10d79626000fa8
lolrafael123 <rafael.antunes2705@gmail.com>
2024-01-14 16:25:47 -0300

    changes

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const pattern = /last_modif\s*=\s*([^;]*)/;
+
+if (
+  Date.parse(document.lastModified) >
+  (parseFloat(document.cookie.match(pattern)?.[1]) || 0)
+) {
+  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${
+    location.pathname
+  }`;
+  alert("This page has changed!");
+}
