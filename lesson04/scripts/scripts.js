268939b1c5a1492d18bd8688eef51d215a459c4f
lolrafael123 <rafael.antunes2705@gmail.com>
2024-02-03 21:15:29 -0300

    chamber

---
+++ lesson04/scripts/scripts.js
@@ -0,0 +1,13 @@
+let text = document.lastModified;
+document.getElementById("last").innerHTML = text;
+
+
+//navBar
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
