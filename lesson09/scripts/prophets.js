711b1c118336ff4c86080ca2df5d2b054d375beb
lolrafael123 <rafael.antunes2705@gmail.com>
2024-03-10 20:14:29 -0300

    changes

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,35 +1,36 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
 
-async function getProphetData(url) {
+async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
+    console.table(data.prophets);
     displayProphets(data.prophets);
 
     const displayProphets = (prophets) => {
         prophets.forEach((prophet) => {
             const card = document.createElement('section');
-            const fullName = document.createElement('_');
+            const fullName = document.createElement('h2');
             const portrait = document.createElement('img');
-    
-            fullName.textContent = `${prophet._____} _____`; 
-           
+
+            fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
             portrait.setAttribute('src', prophet.imageurl);
-            portrait.setAttribute('alt', `Portrait of ${prophet.____} ________`); 
+            portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} ________`);
             portrait.setAttribute('loading', 'lazy');
             portrait.setAttribute('width', '340');
             portrait.setAttribute('height', '440');
-    
-            card.appendChild(____); 
+
+            card.appendChild(fullName);
             card.appendChild(portrait);
             cards.appendChild(card);
-        }); 
-    
+        });
+
     }
-      
+
 }
 
 
-
 getProphetData();
+
+
--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,25 @@
 const baseURL = "https://rafitos123.github.io/wdd230/";
 const linksURL = "https://rafitos123.github.io/wdd230/data/links.json";
-
+const lists = document.querySelector('#links');
 //get results
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
+
+    const displayLinks = (lessons) => {
+        lessons.forEach(link => {
+            const list = document.createElement("ul");
+            const url = document.createElement("li");
+            const Thelink = document.createElement("li");
+
+            list.textContent = `${lessons.title} - ${lessons.url}`;
+        });
+
+        lists.appendChild(list);
+        lists.appendChild(url);
+        lists.appendChild(Thelinklink);
+    }
   }
-  
+
   getLinks();
