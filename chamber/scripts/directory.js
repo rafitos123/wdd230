711af7e16b6d07500ae362039493b1172bff8d43
lolrafael123 <rafael.antunes2705@gmail.com>
2024-03-23 20:18:20 -0300

    changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -14,18 +14,33 @@ async function getMembers() {
             const list = document.createElement("ul");
             const name = document.createElement("li");
             const Url = document.createElement("p");
-            const Phone = document.createElement("li");
+            const Phone = document.createElement("p");
             const Adress = document.createElement("p");
+            const Membership = document.createElement("p");
+            const rate = document.createElement("p");
+            const image = document.createElement('img');
+
+            image.setAttribute('src', company.image);
+            image.setAttribute('alt', `The ${company.name}`); 
+            image.setAttribute('loading', 'lazy');
+            image.setAttribute('width', '100');
+            image.setAttribute('height', '100');
 
             name.textContent = company.name;
             Url.textContent = company.Url;
             Adress.textContent = company.Adress;
             Phone.textContent = company.Phone;
+            Membership.textContent = company.Membership;
+            rate.textContent = company.rate;
+            
 
+            list.appendChild(image);
             list.appendChild(name);
             list.appendChild(Url);
             list.appendChild(Adress);
             list.appendChild(Phone);
+            list.appendChild(Membership);
+            list.appendChild(rate);
             cards.appendChild(list);
         });
 
